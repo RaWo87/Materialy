421aeb2988856dbd1f86a313133d83ce9ec94aae
RaWo87 <woronowicz87@gmail.com>
2019-02-14 23:14:20 +0100

    skonczone klasy

--- Algorytmy & Kata/PodstawyPowtorka/src/com/company/Main.java
+++ Algorytmy & Kata/PodstawyPowtorka/src/com/company/Main.java
@@ -1,11 +1,19 @@
 package com.company;
 
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner input = new Scanner(System.in);
+     String tekst;
+     tekst= input.nextLine();
 
-
+     Integer a = Integer.valueOf(tekst);
+        System.out.println((Integer.valueOf(tekst) instanceof Integer? true:false));
+        System.out.println(a);
 
     }
 }
