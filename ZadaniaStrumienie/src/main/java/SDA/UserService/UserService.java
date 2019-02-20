087f476298d49189a572c77d7ee6f7e743ef3d56
RaWo87 <woronowicz87@gmail.com>
2019-02-20 21:41:36 +0100

    user service skonczony - trzeba tylko pokomentowac

--- ZadaniaStrumienie/src/main/java/SDA/UserService/UserService.java
+++ ZadaniaStrumienie/src/main/java/SDA/UserService/UserService.java
@@ -13,62 +13,79 @@ import java.util.List;
 
 public class UserService {
     static final Logger log = Logger.getLogger(UserService.class.getName());
-    static final String s = Paths.get("").toAbsolutePath().toString().replace("\\","/");
+    static final String s = Paths.get("").toAbsolutePath().toString().replace("\\", "/");
 
-    static final String resourcesPath = s+"/src/main/resources/";
+    static final String resourcesPath = s + "/src/main/resources/";
+
     public void clearLog() throws IOException {
         FileUtils.write(new File(resourcesPath + "StrumienieLogs.log"), "", "UTF-8");
     }
+
     List<User> userzy = new LinkedList<>();
 
     public void readFile() throws IOException {
         clearLog();
 
-        File userActions = new File(s+"\\src\\main\\resources\\userActions.csv");
+        File userActions = new File(s + "\\src\\main\\resources\\userActions.csv");
         LineIterator newLine = FileUtils.lineIterator(userActions, "UTF-8");
 
-        while(newLine.hasNext()){
+        while (newLine.hasNext()) {
 
             String[] line = newLine.nextLine().split(";");
 
-            switch(line[0]){
+            switch (line[0]) {
 
-                case "CREATE":{
+                case "CREATE": {
                     addUser(line);
                     break;
                 }
-                case "UPDATE":{
+                case "UPDATE": {
                     updateUser(line);
                     break;
                 }
-                case "REMOVE":{
+                case "REMOVE": {
                     removeUser(line);
                     break;
                 }
             }
         }
     }
-    public void showUsers(){
-        for (User x: userzy
+
+    public void showUsers() {
+        for (User x : userzy
         ) {
             System.out.println(x.toString());
         }
     }
-    public void addUser(String[] lines){
+
+    public void addUser(String[] lines) {
+        boolean loginTaken = false;
         try {
-            userzy.add(new User(lines[1], lines[2], lines[3], lines[4], lines[5]));
-            log.info("user "+ lines[1]+" ADDed");
-        }catch (Exception e){log.error("User "+lines[1]+" creation failure");}
+            for (User user : userzy
+            ) {
+                if (user.getLogin().equals(lines[1])) {
+                    log.error("login: " + lines[1] + " is Taken");
+                    loginTaken = true;
+                }
+            }
+            if (!loginTaken) {
+                userzy.add(new User(lines[1], lines[2], lines[3], lines[4], lines[5]));
+                log.info("user " + lines[1] + " Created");
+            }
+        } catch (Exception e) {
+            log.error("User " + lines[1] + " creation failure");
+        }
 
     }
+
     public void updateUser(String[] lines) {
         try {
             for (User x : userzy
             ) {
                 if (x.getLogin().equals(lines[1])) {
-                    for (int i = 2; i <lines.length ; i++) {
-                        if (!lines[i].isEmpty()){
-                            switch (i){
+                    for (int i = 2; i < lines.length; i++) {
+                        if (!lines[i].isEmpty()) {
+                            switch (i) {
                                 case 2:
                                     x.setPassword(lines[i]);
                                     break;
@@ -84,9 +101,10 @@ public class UserService {
                             }
                         }
                     }
+                    log.info("user " + lines[1] + " Updated");
                 }
             }
-            log.info("user "+ lines[1]+" updated");
+
         } catch (Exception e) {
             log.error("User " + lines[1] + " update failure");
         }
@@ -94,24 +112,28 @@ public class UserService {
     }
 
 
-
-    public void removeUser(String[] lines){
-        int reference = 0;
-        boolean failSafe = false;
+    public void removeUser(String[] lines) {
+        List<User> garbage = new LinkedList<>();
         try {
-            for (int i = 0; i < userzy.size(); i++) {
-                if(userzy.get(i).getLogin().equals(lines[1])){
-                    reference = i;
-                    failSafe =true;
+
+            for (User x : userzy
+            ) {
+                if (x.getLogin().equals(lines[1])) {
+                    garbage.add(x);
                 }
             }
-            if(failSafe) {
-                userzy.remove(userzy.get(reference));
-                log.info("user " + lines[1] + " removed");
-            }else log.error("User "+lines[1]+" failed to remove");
-            
-        }catch (Exception e){log.error("User "+lines[1]+" failed to remove");}
+            if (garbage.isEmpty()) {
+                log.error(lines[1] + ": no such user");
+            } else {
+                userzy.removeAll(garbage);
+                log.info("user " + lines[1] + " Deleted");
+            }
+        } catch (Exception e) {
+            log.error("User " + lines[1] + " failed to remove");
+        }
     }
+
+
 
 
     }
